9addeed75ff93d7aba8d3e78922b3be7505dabee
BJdeBordeaux <qq870185814@163.com>
2023-11-13 20:08:25 +0100

    add new student functionality

---
+++ tp3/technoWebDistributedFront/src/app/student-requester/student-requester.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-student-requester',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student-requester.component.html',
+  styleUrl: './student-requester.component.css'
+})
+export class StudentRequesterComponent implements OnInit{
+
+
+}
